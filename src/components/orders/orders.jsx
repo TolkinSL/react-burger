a8ece35da1b1feb8b0d354c92c8b08cf25fd46b1
TolkinSL <tolkinsl@yandex.ru>
2023-08-20 16:38:08 +0300

    add WebSockets

--- src/components/orders/orders.jsx
+++ src/components/orders/orders.jsx
@@ -1,4 +1,24 @@
+import {connect, disconnect} from "../../services/actions/websocket-slice";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {getCookie} from "../../utils/cookie";
+
 const Orders = () => {
+    const dispatch = useDispatch();
+    const orders = useSelector(store => store.wsData.orders);
+    const token = getCookie("accessToken").split(' ')[1];
+    //const connect = () => dispatch(connect('wss://norma.nomoreparties.space/orders/all'));
+    //const disconnect = () => dispatch(disconnect());
+    console.log('WsOrder');
+    console.log(orders);
+
+    useEffect(() => {
+        dispatch(connect(`wss://norma.nomoreparties.space/orders?token=${token}`));
+        return () => {
+            dispatch(disconnect());
+        }
+    }, [dispatch]);
+
     return (
       <>
           <p>Заказы</p>
--- src/pages/feed/feed.jsx
+++ src/pages/feed/feed.jsx
@@ -1,4 +1,22 @@
+import {connect, disconnect} from "../../services/actions/websocket-slice";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+
 const Feed = () => {
+    const dispatch = useDispatch();
+    const orders = useSelector(store => store.wsData.orders);
+    //const connect = () => dispatch(connect('wss://norma.nomoreparties.space/orders/all'));
+    //const disconnect = () => dispatch(disconnect());
+    console.log('WsOrder');
+    console.log(orders);
+
+    useEffect(() => {
+        dispatch(connect('wss://norma.nomoreparties.space/orders/all'));
+        return () => {
+            dispatch(disconnect());
+        }
+    }, [dispatch]);
+
     return (
       <>
           <p>Лента Заказов</p>
